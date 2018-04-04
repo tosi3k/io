2ee4aa570984ffefc36444f646becd4285a6a2cf
tosi3k <antek.zaw@gmail.com>
2018-04-04 15:15:37 +0200

    add models for station and path

--- ioioio/rest_api/models.py
+++ ioioio/rest_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Station(models.Model):
+    name      = models.CharField(max_length=64)
+    latitude  = models.CharField(max_length=16)
+    longitude = models.CharField(max_length=16)
+
+class Path(models.Model):
+    time      = models.IntegerField()
+    station_a = models.ForeignKey(Station, related_name='station_a', on_delete=models.CASCADE)
+    station_b = models.ForeignKey(Station, related_name='station_b', on_delete=models.CASCADE)
