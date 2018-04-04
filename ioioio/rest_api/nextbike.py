0ece4ee7d9ca18687706d61fae79de2d6e604aba
tosi3k <antek.zaw@gmail.com>
2018-04-04 19:06:18 +0200

    add fetching non-empty stations

--- ioioio/rest_api/nextbike.py
+++ ioioio/rest_api/nextbike.py
@@ -1,5 +1,5 @@
 import urllib.request
-from rest_api.models import Station
+from .models import Station
 import xml.etree.ElementTree as ET
 
 NEXTBIKE_URL = 'http://api.nextbike.net/maps/nextbike-official.xml?city=210'
@@ -9,10 +9,18 @@ def nextbike_xml():
     tree = ET.fromstring(xml_contents)
     return tree
 
-def import_stations():
+def stations_with_bike():
     xml = nextbike_xml()
+    names = []
     for place in xml[0][0]:
-        s = Station(name=place.attrib['name'],
-                    latitude=place.attrib['lat'],
-                    longitude=place.attrib['lng'])
-        s.save()
+        if int(place.attrib['bikes']):
+            names.append(place.attrib['name'])
+    return Station.objects.filter(name__in=names)
+
+# def import_stations():
+#     xml = nextbike_xml()
+#     for place in xml[0][0]:
+#         s = Station(name=place.attrib['name'],
+#                     latitude=place.attrib['lat'],
+#                     longitude=place.attrib['lng'])
+#         s.save()
