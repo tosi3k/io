baaf3dffbdbf36e1e9fa50ac9af1fd098498c7dc
Michał <mi.oltarzewski@gmail.com>
2018-05-21 16:35:29 +0200

    Production version initial

--- ioioio/ioioio/settings.py
+++ ioioio/ioioio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^r^9ahldtvv49dj)0i8l_zt_w*-pzlfmy#du8q1^4$3)p$4&oh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '77.55.218.173']
+ALLOWED_HOSTS = ['friturillo.pl', '0.0.0.0', '127.0.0.1', 'localhost', '77.55.218.173']
 
 
 # Application definition
@@ -75,10 +75,6 @@ WSGI_APPLICATION = 'ioioio.wsgi.application'
 
 # CORS Headers
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#     'localhost:3000',
-# )
-
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
@@ -128,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
